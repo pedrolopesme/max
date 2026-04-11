0be0161f657b97d2e70a73e7c4639c80bc34bfd4
Pedro Mendes <pedrolopesme@gmail.com>
2026-04-11 20:09:20 -0300

    minor: entering raw mode

--- max.c
+++ max.c
@@ -1,2 +1,62 @@
+/*** last chapter:
+ * https://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html  ***/
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
 
-int main(void) { return 0; }
+struct termios original_term;
+
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
+void disableRawMode(void) {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_term) == -1) {
+    die("tcsetattr");
+  }
+}
+
+void enableRawMode(void) {
+  if (tcgetattr(STDIN_FILENO, &original_term) == -1) {
+    die("tcgetattr");
+  }
+  atexit(disableRawMode);
+
+  struct termios raw = original_term;
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+    die("tcsetattr");
+  }
+}
+
+int main(void) {
+  enableRawMode();
+
+  while (1) {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+      die("read");
+    }
+
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+
+    if (c == 'q')
+      break;
+  }
+
+  return 0;
+}
